53c11750c4989afa256cb627df9510e0c7960be2
Peter <peter.budo@gmail.com>
2013-10-13 19:47:09 +0100

    - Fixed issues with Simple XML library, default setup crashes Android build (http://stackoverflow.com/a/18591623/209574) - Skeleton of Android app

--- app/src/main/java/com/petermiklosko/rssreader/RSSActivity.java
+++ app/src/main/java/com/petermiklosko/rssreader/RSSActivity.java
@@ -1,13 +1,17 @@
 package com.petermiklosko.rssreader;
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.widget.ListView;
 
-public class RSSActivity extends Activity {
+public class RSSActivity extends FragmentActivity {
+
+    private ListView feedsListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.rss_activity);
+        feedsListView = (ListView) findViewById(R.id.feeds_list);
     }
 }
